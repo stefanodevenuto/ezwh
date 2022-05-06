d4cf9643a488e31651d20e84757f2ecab926ec90
Stefano De Venuto <s304140@studenti.polito.it>
2022-05-06 13:53:47 +0200

    Adds optimized in-memory cache (conditionally enabled)

--- code/server/api/components/index.js
+++ code/server/api/components/index.js
@@ -1,6 +1,7 @@
 
 // import { AuthRoutes } from './auth/routes';
 var SkuRoutes = require('./sku/routes');
+var express = require('express');
 
 function registerApiRoutes(router, prefix = '') {
 	//router.use(`${prefix}/auth`, new AuthRoutes().router);
@@ -9,13 +10,17 @@ function registerApiRoutes(router, prefix = '') {
 	const skuRoute = new SkuRoutes();
 	
 	// Init all Maps
-	const skuReady = skuRoute.initMap();
+	if (process.env.ENABLE_MAP === "true") {
+		const skuReady = skuRoute.initMap();
+		// const used = process.memoryUsage().heapUsed / 1024 / 1024;
+		// console.log(`Memory used: ${used}MB`)
 
-	// Wait all the Maps to setup
-	Promise.all([skuReady]).catch(err => {
-		throw err;
-	});
-
+		// Wait all the Maps to setup
+		Promise.all([skuReady]).catch(err => {
+			throw err;
+		});
+	}
+	
 	router.use(`${prefix}/sku`, skuRoute.router);
 	router.use(`${prefix}/skus`, skuRoute.router);
 	
--- code/server/api/components/sku/controller.js
+++ code/server/api/components/sku/controller.js
@@ -1,31 +1,42 @@
 const SkuDAO = require('./dao')
 const Sku = require("./sku");
 const { SkuErrorFactory } = require('./error');
+const Cache = require('lru-cache')
+//const sizeof = require('object-sizeof')
 
 class SkuController {
-	constructor(enableMap) {
+	constructor() {
 		this.dao = new SkuDAO();
-		this.skuMap = new Map();
-		this.enableMap = enableMap;
+		this.skuMap = new Cache({ max: Number(process.env.EACH_MAP_CAPACITY) });
+		this.enableCache = (process.env.ENABLE_MAP === "true") || false;
+		this.allInCache = false;
 	}
 
 	async initMap() {
 		const allSku = await this.dao.getAllSkus()
-							.catch(() => { throw SkuErrorFactory.initializeMapFailed() });
-		
-		allSku.map((sku) => this.skuMap.set(sku.id, sku));
+			.catch(() => { throw SkuErrorFactory.initializeMapFailed() });
+
+		if (this.enableCache && allSku.length < this.skuMap.max) {
+			allSku.map((sku) => this.skuMap.set(sku.id, sku));
+			this.allInCache = true;
+		}
 	}
 
 	async getAllSkus(req, res, next) {
 		try {
-			if (this.enableMap) {
+			if (this.enableCache && this.allInCache) {
 				const allSku = Array.from(this.skuMap.values());
 				return res.status(200).json(allSku);
 			}
 
 			const rows = await this.dao.getAllSkus();
-			const skus = rows.map(record => new Sku(record.id, record.description, record.weight, record.volume, record.notes, 
-													record.position, record.availableQuantity, record.price));
+			const skus = rows.map(record => new Sku(record.id, record.description, record.weight, record.volume, record.notes,
+				record.position, record.availableQuantity, record.price));
+
+			if (this.enableCache && rows.length < this.skuMap.max) {
+				allSku.map((sku) => this.skuMap.set(sku.id, sku));
+				this.allInCache = true;
+			}
 
 			return res.status(200).json(skus);
 		} catch (err) {
@@ -35,28 +46,24 @@ class SkuController {
 
 	async getSkuByID(req, res, next) {
 		try {
-			const skuId = req.params.id;
+			const skuId = Number(req.params.id);
 
-			if (this.enableMap) {
+			if (this.enableCache) {
 				const sku = this.skuMap.get(Number(skuId));
 
 				if (sku !== undefined)
 					return res.status(200).json(sku);
-
-				// Do we trust the Map this much to let it rule? 
-				/*
-				if (sku === undefined)
-					throw SkuErrorFactory.newSkuNotFound();
-				return res.status(200).json(sku);*/
 			}
 
 			const row = await this.dao.getSkuByID(skuId);
-
 			if (row === undefined)
 				throw SkuErrorFactory.newSkuNotFound();
 
-			const sku = new Sku(row.id, row.description, row.weight, row.volume, row.notes, 
-								row.position, row.availableQuantity, row.price);
+			const sku = new Sku(row.id, row.description, row.weight, row.volume, row.notes,
+				row.position, row.availableQuantity, row.price);
+
+			if (this.enableCache)
+				this.skuMap.set(sku.id, sku)
 
 			return res.status(200).json(sku);
 		} catch (err) {
@@ -68,10 +75,10 @@ class SkuController {
 		try {
 			const rawSku = req.body;
 			const { id } = await this.dao.createSku(rawSku);
-			
-			if (this.enableMap) {
-				const sku = new Sku(id, rawSku.description, rawSku.weight, rawSku.volume, 
-									rawSku.notes, null, rawSku.availableQuantity, rawSku.price);
+
+			if (this.enableCache) {
+				const sku = new Sku(id, rawSku.description, rawSku.weight, rawSku.volume,
+					rawSku.notes, null, rawSku.availableQuantity, rawSku.price);
 
 				this.skuMap.set(Number(id), sku);
 			}
@@ -87,7 +94,7 @@ class SkuController {
 			const skuId = req.params.id;
 			const rawSku = req.body;
 
-			if (this.enableMap) {
+			if (this.enableCache) {
 				let sku = this.skuMap.get(Number(skuId));
 				sku.description = rawSku.newDescription;
 				sku.weight = rawSku.newWeight;
@@ -97,11 +104,8 @@ class SkuController {
 				sku.availableQuantity = rawSku.newAvailableQuantity;
 			}
 
-			// Do we trust the Map this much to let it rule? (without await, modification postponed)
-			// this.dao.modifySku(skuId, rawSku);
-			
 			await this.dao.modifySku(skuId, rawSku);
-			
+
 			return res.status(200).send();
 		} catch (err) {
 			return next(err);
@@ -113,16 +117,13 @@ class SkuController {
 			const skuId = req.params.id;
 			const newPosition = req.body.position;
 
-			if (this.enableMap) {
+			if (this.enableCache) {
 				let sku = this.skuMap.get(Number(skuId));
 				sku.position = newPosition;
 			}
 
-			// Do we trust the Map this much to let it rule? (without await, modification postponed)
-			// this.dao.addModifySkuPosition(skuId, newPosition);
-
 			await this.dao.addModifySkuPosition(skuId, newPosition);
-			
+
 			return res.status(200).send();
 		} catch (err) {
 			return next(err);
@@ -133,14 +134,12 @@ class SkuController {
 		try {
 			const skuId = req.params.id;
 
-			if (this.enableMap) {
+			if (this.enableCache) {
 				this.skuMap.delete(Number(skuId));
 			}
 
-			// Do we trust the Map this much to let it rule? (without await, modification postponed)
-			// this.dao.deleteSku(skuId);
 			await this.dao.deleteSku(skuId);
-			
+
 			return res.status(204).send();
 		} catch (err) {
 			return next(err);
