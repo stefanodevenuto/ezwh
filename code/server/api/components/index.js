f5288df3065524d2aa3a89863b4edc0608c57b7b
Roberto Sirigu <s303346@studenti.polito.it>
2022-05-11 15:20:46 +0200

    Update index.js, introducing the observer pattern

--- code/server/api/components/index.js
+++ code/server/api/components/index.js
@@ -1,29 +1,26 @@
-
 // import { AuthRoutes } from './auth/routes';
-var SkuRoutes = require('./sku/routes');
 var express = require('express');
 
+var SkuRoutes = require('./sku/routes');
+var ItemRoutes = require('./item/routes');
+
 function registerApiRoutes(router, prefix = '') {
-	//router.use(`${prefix}/auth`, new AuthRoutes().router);
 
 	// Create all Routes
 	const skuRoute = new SkuRoutes();
+	const itemRoute = new ItemRoutes();
 	
-	// Init all Maps
+	// Set the Observable-Observe pattern
 	if (process.env.ENABLE_MAP === "true") {
-		const skuReady = skuRoute.initMap();
-		// const used = process.memoryUsage().heapUsed / 1024 / 1024;
-		// console.log(`Memory used: ${used}MB`)
-
-		// Wait all the Maps to setup
-		Promise.all([skuReady]).catch(err => {
-			throw err;
-		});
+		itemRoute.controller.addObserver(itemRoute.controller);
+		skuRoute.controller.addObserver(positionRoute.controller);
 	}
 	
+	// Set all Routes
 	router.use(`${prefix}/sku`, skuRoute.router);
 	router.use(`${prefix}/skus`, skuRoute.router);
-	
+	router.use(`${prefix}/item`, itemRoute.router);
+	router.use(`${prefix}/items`, itemRoute.router);
 }
 
 module.exports = registerApiRoutes;
